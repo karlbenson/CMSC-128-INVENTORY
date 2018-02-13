a6f7c4348d65ccfad24fc96fd7d4971a54541312
Karl Mabutas <31471891+karlbenson@users.noreply.github.com>
2018-02-14 01:19:01 +0800

    Minor Change

--- js/sideMenuBar.js
+++ js/sideMenuBar.js
@@ -11,6 +11,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
         document.getElementById("myBtn").style.display = "block";
+        
     } else {
         document.getElementById("myBtn").style.display = "none";
     }
