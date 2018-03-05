835db4b728e89d96b7c844a4fd9b6a8409db4b81
Karl Mabutas <31471891+karlbenson@users.noreply.github.com>
2018-03-05 21:37:47 +0800

    [STREAMLINE] Bootstrap link

--- login.php
+++ login.php
@@ -5,10 +5,11 @@
 	<title>Log in: UPB Glasswares and Chemicals Invetory</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="css/bootstrap.min2.css">
-	<link href="css/global.css" rel="stylesheet">
-	<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-	<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>	
+	<link rel="stylesheet" href="css/global.css" >
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	
+	<script src='js/jquery-3.3.1.slim.min.js'></script>
+	<script src='js/bootstrap.min.js'></script>	
 </head>
 <body>
 
@@ -39,7 +40,7 @@
 		</div>
 		<div class="checkbox">
 			<label>
-				<input type="checkbox"> Remember me
+				<input title="Kapag nag-iisa~" type="checkbox"> Remember me
 			</label>
 		</div>
 		<button type="submit" class="btn btn-success btn-block"  name="Login">LOG IN</button>
