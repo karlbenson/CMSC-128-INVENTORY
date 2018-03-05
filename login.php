6378ad4a07eb1c1c04c26246cd25a214adcf8059
Karl Mabutas <31471891+karlbenson@users.noreply.github.com>
2018-03-05 22:32:36 +0800

    [MINOR] Re-CSS

--- login.php
+++ login.php
@@ -14,36 +14,38 @@
 <body>
 
 <div class="container-fluid bg">
-	<div class="container-fluid topmost">
-		<div class="row">
-		<a href="login.php"><img src="src/up.png" width="120px" class="center"></a>
-		</div>
+	<div class="container-fluid" style="text-align: center; padding-top: 1%; margin-bottom: 1%;"><a href="home.php"><img id=uplogo src="src/up.png" width="150px"></a></div>
+	<div class="row">
+			<div class="col-sm-12 stack container-fluid">
+				<div style="height: 70px padding-bottom:20px;"><h2>University of The Philippines - Baguio</h2></div>
+				<span class="hidden-sm-down">
+					<div><h3>College of Science</h3></div>
+					<div><h4>Glasswares and Chemicals Inventory</h4></div>
+				</span>
+
+			</div>
 	</div>
 
 	<div class="row">
-	<h2 class="mid">University of The Philippines - Baguio</h2>
-	<!--<h3 class="mid">University of the Philippines</h3>-->
-	<h3 class="mid sec">College of Science <br/>Glasswares and Chemicals Inventory</h3>
 	<div class="col-md-4 col-sm-4 col-xs-12"></div>
 	<div class="col-md-4 col-sm-4 col-xs-12">
 	<h3><!--LOG IN--></h3>
 	<!--start of form-->
-		<form class="form-container">
-		
-		<div class="form-group">
-			<label for="exampleInputEmail1">USERNAME</label>
-			<input type="text" name="username" class="form-control" id="exampleInputUsername1" placeholder="Username" required>
-		</div>
-		<div class="form-group">
-			<label for="exampleInputPassword1">PASSWORD</label>
-			<input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
-		</div>
-		<div class="checkbox">
-			<label>
-				<input title="Kapag nag-iisa~" type="checkbox"> Remember me
-			</label>
-		</div>
-		<button type="submit" class="btn btn-success btn-block"  name="Login">LOG IN</button>
+		<form class="form-container chenelin" style="margin-top:5%; margin-bottom: 4%;">
+			<div class="form-group">
+				<label for="exampleInputEmail1">USERNAME</label>
+				<input type="text" name="username" class="form-control" id="exampleInputUsername1" placeholder="Username" required>
+			</div>
+			<div class="form-group">
+				<label for="exampleInputPassword1">PASSWORD</label>
+				<input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+			</div>
+			<div class="checkbox">
+				<label>
+					<input title="Kapag nag-iisa~" type="checkbox"> Remember me
+				</label>
+			</div>
+			<button type="submit" class="btn btn-success btn-block"  name="Login">LOG IN</button>
 		</form>
 	<!--end of form-->
 	</div>
