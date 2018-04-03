0880af1b175ce3ad1f59aeb37e6b18652e95b873
Karl Mabutas <31471891+karlbenson@users.noreply.github.com>
2018-04-04 01:28:02 +0800

    slightly better go to top button bc why not

--- js/sideMenuBar.js
+++ js/sideMenuBar.js
@@ -10,10 +10,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        document.getElementById("myBtn").style.display = "block";
+        document.getElementById("myBtnh").style.display = "block";
         
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        document.getElementById("myBtnh").style.display = "none";
     }
 }
 
