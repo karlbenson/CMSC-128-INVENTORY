a7c6286100cfcba3525a8175ca8a9c2b3d368ffc
Kiana Alessandra Villaera <kvvillaera@up.edu.ph>
2018-04-03 20:18:42 +0800

    DB changes ulit. Huhu sorry guys :(

--- chem_glasswares.sql
+++ chem_glasswares.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2018 at 01:39 PM
+-- Generation Time: Apr 03, 2018 at 02:17 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -29,40 +29,23 @@ SET time_zone = "+00:00";
 CREATE TABLE `borrower` (
   `Borrower_Id` int(11) NOT NULL,
   `First_Name` varchar(256) NOT NULL,
-  `Last_Name` varchar(256) NOT NULL
+  `Last_Name` varchar(256) NOT NULL,
+  `Student_Number` varchar(20) NOT NULL,
+  `Amt_of_transactions` int(11) NOT NULL,
+  `Group_Id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `borrower`
 --
 
-INSERT INTO `borrower` (`Borrower_Id`, `First_Name`, `Last_Name`) VALUES
-(1, 'Kiana Alessandra ', 'Villaera'),
-(2, 'Ralston Mark', 'Chan'),
-(3, 'Karl Vinzon', 'Mabutas'),
-(4, 'Bernadette', 'Genove');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `borrower_group`
---
-
-CREATE TABLE `borrower_group` (
-  `Borrower_Group_Id` int(11) NOT NULL,
-  `Group_Id` int(11) NOT NULL,
-  `Borrower_Id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `borrower_group`
---
-
-INSERT INTO `borrower_group` (`Borrower_Group_Id`, `Group_Id`, `Borrower_Id`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 2, 3),
-(4, 2, 4);
+INSERT INTO `borrower` (`Borrower_Id`, `First_Name`, `Last_Name`, `Student_Number`, `Amt_of_transactions`, `Group_Id`) VALUES
+(1, 'Kiana Alessandra ', 'Villaera', '2014-15055', 1, 1),
+(2, 'Karl Vinzon', 'Mabutas', '2015-12345', 1, 1),
+(3, 'Bernadette', 'Genove', '2015-54321', 1, 2),
+(4, 'Ralston Mark', 'Chan', '2015-78945', 1, 2),
+(5, 'Karissa Isabel', 'Patriarca', '2016-52416', 1, 3),
+(6, 'Paul', 'Kline', '2007-85246', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -74,17 +57,19 @@ CREATE TABLE `chemicals` (
   `Chemical_Id` int(11) NOT NULL,
   `Name` varchar(256) NOT NULL,
   `Quantity_Available_ml` float NOT NULL,
-  `Quantity_Available_mg` float NOT NULL
+  `Quantity_Available_mg` float NOT NULL,
+  `Original_Amt` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `chemicals`
 --
 
-INSERT INTO `chemicals` (`Chemical_Id`, `Name`, `Quantity_Available_ml`, `Quantity_Available_mg`) VALUES
-(4, 'trulalu', 35, 0),
-(6, 'Jalapeno Sauce', 100, 0),
-(25, 'Good stuff', 0, 5);
+INSERT INTO `chemicals` (`Chemical_Id`, `Name`, `Quantity_Available_ml`, `Quantity_Available_mg`, `Original_Amt`) VALUES
+(26, 'Good stuff ', 0, 50.44, 50),
+(27, 'Air Force Liquiblunt', 20.12, 0, 20.12),
+(28, 'We Stan Good Fumes', 0, 14.2, 14.2),
+(29, 'Unstan Milktea', 74.53, 0, 74.53);
 
 -- --------------------------------------------------------
 
@@ -103,8 +88,8 @@ CREATE TABLE `glasswares` (
 --
 
 INSERT INTO `glasswares` (`Glassware_Id`, `Name`, `Quantity_Available`) VALUES
-(1, 'BONG BONG MARCOS', 6),
-(2, 'Hammer', 3),
+(1, 'Bong Pok Marcos', 4),
+(2, 'Angel''s Burger MayoChup Tarpaulin', 10),
 (3, 'Lofi Stereo', 2);
 
 -- --------------------------------------------------------
@@ -124,8 +109,9 @@ CREATE TABLE `group_table` (
 --
 
 INSERT INTO `group_table` (`Group_Id`, `Professor`, `Subject`) VALUES
-(1, 'Sir Lee Javellana', 'Cmsc 128'),
-(2, 'Ma''am Ash Balangcod', 'Cmsc 125');
+(1, 'Sir Lee Javellana', 'Cmsc 125'),
+(2, 'Ma''am Ashlyn Balangcod', 'Cmsc 128'),
+(3, 'Sir Martin Roy Nabus', 'Cmsc 130');
 
 -- --------------------------------------------------------
 
@@ -180,8 +166,9 @@ CREATE TABLE `transaction` (
 --
 
 INSERT INTO `transaction` (`Trans_Id`, `Glassware_Id`, `Chemical_Id`, `Group_Id`, `Qty_Borrowed_Glasswares`, `Qty_Borrowed_Chemicals_ml`, `Qty_Borrowed_Chemicals_mg`, `Date_Borrowed`, `Date_Returned`) VALUES
-(5, 1, NULL, 1, 1, 0, 0, '2018-04-08', '2018-04-13'),
-(6, NULL, 4, 2, 0, 4, 0, '2018-03-12', '2018-03-16');
+(1, 1, NULL, 1, 2, 0, 0, '2018-04-01', '2018-04-08'),
+(2, 2, NULL, 2, 1, 0, 0, '2018-03-19', '2018-03-21'),
+(3, NULL, 26, 3, 0, 0, 5.2, '2018-02-14', '2018-02-14');
 
 -- --------------------------------------------------------
 
@@ -212,15 +199,8 @@ INSERT INTO `user_accounts` (`User_Id`, `Staff_Id`, `Username`, `Password`, `sta
 -- Indexes for table `borrower`
 --
 ALTER TABLE `borrower`
-  ADD PRIMARY KEY (`Borrower_Id`);
-
---
--- Indexes for table `borrower_group`
---
-ALTER TABLE `borrower_group`
-  ADD PRIMARY KEY (`Borrower_Group_Id`),
-  ADD KEY `Group_Id` (`Group_Id`),
-  ADD KEY `Borrower_Id` (`Borrower_Id`);
+  ADD PRIMARY KEY (`Borrower_Id`),
+  ADD KEY `Group_Id` (`Group_Id`);
 
 --
 -- Indexes for table `chemicals`
@@ -277,17 +257,12 @@ ALTER TABLE `user_accounts`
 -- AUTO_INCREMENT for table `borrower`
 --
 ALTER TABLE `borrower`
-  MODIFY `Borrower_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `borrower_group`
---
-ALTER TABLE `borrower_group`
-  MODIFY `Borrower_Group_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Borrower_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `chemicals`
 --
 ALTER TABLE `chemicals`
-  MODIFY `Chemical_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `Chemical_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `glasswares`
 --
@@ -297,7 +272,7 @@ ALTER TABLE `glasswares`
 -- AUTO_INCREMENT for table `group_table`
 --
 ALTER TABLE `group_table`
-  MODIFY `Group_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Group_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `staff`
 --
@@ -307,7 +282,7 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `Trans_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `Trans_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user_accounts`
 --
@@ -318,11 +293,10 @@ ALTER TABLE `user_accounts`
 --
 
 --
--- Constraints for table `borrower_group`
+-- Constraints for table `borrower`
 --
-ALTER TABLE `borrower_group`
-  ADD CONSTRAINT `borrower_group_ibfk_1` FOREIGN KEY (`Group_Id`) REFERENCES `group_table` (`Group_Id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `borrower_group_ibfk_2` FOREIGN KEY (`Borrower_Id`) REFERENCES `borrower` (`Borrower_Id`) ON UPDATE CASCADE;
+ALTER TABLE `borrower`
+  ADD CONSTRAINT `borrower_ibfk_1` FOREIGN KEY (`Group_Id`) REFERENCES `group_table` (`Group_Id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `page_permissions`
