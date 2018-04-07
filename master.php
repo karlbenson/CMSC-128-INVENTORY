9d84071795a08b0fdac648c7fe04a9302dd0e502
Karl Mabutas <31471891+karlbenson@users.noreply.github.com>
2018-04-08 01:15:54 +0800

    datatables plugin implementation on master

--- master.php
+++ master.php
@@ -23,17 +23,18 @@
 	<link rel="stylesheet" href="css/master.css">
 	<link rel="stylesheet" href="css/modal.css">
 	<link rel="stylesheet" href="css/font-awesome.min.js">
-	<link rel="stylesheet" href="datatables/datatables.css">
-	<script type="text/javascript" charset="utf8" src="datatables/datatables.js"></script>
+	<link rel="stylesheet" href="datatables/DataTables/css/dataTables.bootstrap4.css">
 	<script src="js/jquery.min.js"></script>
+	<script src="datatables/DataTables/js/jquery.dataTables.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" charset="utf8" src="datatables/DataTables/js/dataTables.bootstrap4.js"></script>
 	<?php 'loading head';include("head.php"); ?>
 </head>
 <body>
 	<div id="container-fluid">
 		<h1 class="jumbotron-fluid text-center py-4" style="font-size: 50px"><em>Master List</h1>
 		<div id="id01" class="w3-modal" style="z-index:9999;">
-			<div class="w3-modal-content" style="border-radius: 10px; padding: 20px;">
+			<div class="w3-modal-content w3-animate-bottom" style="border-radius: 10px; padding: 20px;">
 				<header class="w3-container" style="text-align: center;"> 
 					<button onclick="document.getElementById('id01').style.display='none'" 
 					class="btn btn-danger btn-lg"><i class="fas fa-times"></i></button>
@@ -85,7 +86,7 @@
 	    		<div class="container">
 			    	<div class="row">
 			        	<div class="col-md-12">
-			          		<table class="table table-striped table-condensed table-hover" id="table_id">
+			          		<table class="table table-sm table-striped table-condensed table-hover" id="table_id">
 			          			<thead class="text-center">
 					            	<tr>
 						                <th>ID</th>
@@ -106,10 +107,10 @@
 											while ($MyResults = $MyValues -> fetch_assoc())
 											{
 												echo '<tr>';
-												echo '<td>'.$MyResults['Chemical_Id'].'</td>';
-												echo '<td>'.$MyResults['Name'].'</td>';
-												echo '<td>'.$MyResults['Quantity_Available_mg'].'</td>';
-												echo '<td>'.$MyResults['Quantity_Available_ml'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Chemical_Id'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Name'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Quantity_Available_mg'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Quantity_Available_ml'].'</td>';
 												echo '<td>
 												<form method="POST" action = "edit_chemical_item.php">
 												<input type="hidden" class="hide" placeholder="'.$MyResults['Chemical_Id'].'" value="'.$MyResults['Chemical_Id'].'" name="Chemical_Id" readonly>
@@ -141,7 +142,7 @@
 	    		<div class="container">
 			    	<div class="row">
 			        	<div class="col-md-12">
-			          		<table class="table table-striped table-condensed table-hover" id="table_id">
+			          		<table class="table table-sm table-striped table-condensed table-hover" id="table_id2">
 			          			<thead class="text-center">
 					            	<tr>
 						                <th>ID</th>
@@ -161,9 +162,9 @@
 											while ($MyResults = $MyValues -> fetch_assoc())
 											{
 												echo '<tr>';
-												echo '<td>'.$MyResults['Glassware_Id'].'</td>';
-												echo '<td>'.$MyResults['Name'].'</td>';
-												echo '<td>'.$MyResults['Quantity_Available'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Glassware_Id'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Name'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Quantity_Available'].'</td>';
 											
 												echo '<td>
 												<form method="POST" action = "edit_glassware_item.php">
@@ -201,7 +202,7 @@
 	    		<div class="container">
 			    	<div class="row">
 			        	<div class="col-md-12">
-			          		<table class="table table-striped table-condensed table-hover" id="table_id">
+			          		<table class="table table-sm table-striped table-condensed table-hover" id="table_id3">
 			          			<thead class="text-center">
 					            	<tr>
 						                <th>ID</th>
@@ -222,10 +223,10 @@
 											while ($MyResults = $MyValues -> fetch_assoc())
 											{
 												echo '<tr>';
-												echo '<td>'.$MyResults['Chemical_Id'].'</td>';
-												echo '<td>'.$MyResults['Name'].'</td>';
-												echo '<td>'.$MyResults['Quantity_Available_mg'].'</td>';
-												echo '<td>'.$MyResults['Quantity_Available_ml'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Chemical_Id'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Name'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Quantity_Available_mg'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Quantity_Available_ml'].'</td>';
 											
 												echo '<td>
 												<form method="POST" action = "edit_chemical_item.php">
@@ -262,7 +263,7 @@
 	    		<div class="container">
 			    	<div class="row">
 			        	<div class="col-md-12">
-			          		<table class="table table-striped table-condensed table-hover" id="table_id">
+			          		<table class="table table-sm table-striped table-condensed table-hover" id="table_id4">
 			          			<thead class="text-center">
 					            	<tr>
 						                <th>ID</th>
@@ -282,9 +283,9 @@
 											while ($MyResults = $MyValues -> fetch_assoc())
 											{
 												echo '<tr>';
-												echo '<td>'.$MyResults['Glassware_Id'].'</td>';
-												echo '<td>'.$MyResults['Name'].'</td>';
-												echo '<td>'.$MyResults['Quantity_Available'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Glassware_Id'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Name'].'</td>';
+												echo '<td class="align-middle">'.$MyResults['Quantity_Available'].'</td>';
 											
 												echo '<td>
 												<form method="POST" action = "edit_glassware_item.php">
@@ -375,11 +376,49 @@
 	jQuery(function(){
 	   jQuery('#allbtn').click();
 	});
-	</script>
-	<script>
-		$(document).ready( function () {
-		    $('#table_id').DataTable();
-		} );
+
+	$('#table_id').DataTable({
+			"columns": [
+			null,
+			null,
+		    null,
+		    null,
+			
+		    { "orderable": false },
+		    { "orderable": false }
+  			]
+	});
+	$('#table_id2').DataTable({
+			"columns": [
+			null,
+			null,
+		    null,
+			
+		    { "orderable": false },
+		    { "orderable": false }
+  			]
+	});
+	$('#table_id3').DataTable({
+			"columns": [
+			null,
+			null,
+		    null,
+		    null,
+			
+		    { "orderable": false },
+		    { "orderable": false }
+  			]
+	});
+	$('#table_id4').DataTable({
+			"columns": [
+			null,
+			null,
+		    null,
+			
+		    { "orderable": false },
+		    { "orderable": false }
+  			]
+	});
 	</script>
 </body>
 </html>
