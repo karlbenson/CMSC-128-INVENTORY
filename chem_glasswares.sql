0edce114036b197a6da973aae3ec7b0ad869fbfe
Kiana Alessandra Villaera <kvvillaera@up.edu.ph>
2018-04-12 19:10:29 +0800

    Legit-ish data entries

--- chem_glasswares.sql
+++ chem_glasswares.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2018 at 08:05 AM
+-- Generation Time: Apr 12, 2018 at 01:09 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -29,40 +29,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `borrower` (
   `Borrower_Id` int(11) NOT NULL,
   `First_Name` varchar(256) NOT NULL,
-  `Last_Name` varchar(256) NOT NULL
+  `Last_Name` varchar(256) NOT NULL,
+  `Student_Number` varchar(20) NOT NULL,
+  `Amt_of_transactions` int(11) NOT NULL,
+  `Group_Id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `borrower`
 --
 
-INSERT INTO `borrower` (`Borrower_Id`, `First_Name`, `Last_Name`) VALUES
-(1, 'Kiana Alessandra ', 'Villaera'),
-(2, 'Ralston Mark', 'Chan'),
-(3, 'Karl Vinzon', 'Mabutas'),
-(4, 'Bernadette', 'Genove');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `borrower_group`
---
-
-CREATE TABLE `borrower_group` (
-  `Borrower_Group_Id` int(11) NOT NULL,
-  `Group_Id` int(11) NOT NULL,
-  `Borrower_Id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `borrower_group`
---
-
-INSERT INTO `borrower_group` (`Borrower_Group_Id`, `Group_Id`, `Borrower_Id`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 2, 3),
-(4, 2, 4);
+INSERT INTO `borrower` (`Borrower_Id`, `First_Name`, `Last_Name`, `Student_Number`, `Amt_of_transactions`, `Group_Id`) VALUES
+(1, 'Kiana Alessandra', 'Villaera', '2014-15055', 1, 1),
+(2, 'Karlvinzon', 'Mabutas', '2015-12345', 1, 1),
+(3, 'Bernadette', 'Genove', '2015-54321', 0, 2),
+(4, 'Ralston Mark', 'Chan', '2015-35142', 0, 2);
 
 -- --------------------------------------------------------
 
@@ -73,18 +54,22 @@ INSERT INTO `borrower_group` (`Borrower_Group_Id`, `Group_Id`, `Borrower_Id`) VA
 CREATE TABLE `chemicals` (
   `Chemical_Id` int(11) NOT NULL,
   `Name` varchar(256) NOT NULL,
-  `Quantity_Available_ml` float NOT NULL,
-  `Quantity_Available_mg` float NOT NULL
+  `Quantity_Available_ml` float DEFAULT NULL,
+  `Quantity_Available_mg` float DEFAULT NULL,
+  `Original_Amt` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `chemicals`
 --
 
-INSERT INTO `chemicals` (`Chemical_Id`, `Name`, `Quantity_Available_ml`, `Quantity_Available_mg`) VALUES
-(4, 'trulalu', 35, 0),
-(6, 'Jalapeno Sauce', 100, 0),
-(25, 'Good stuff', 0, 5);
+INSERT INTO `chemicals` (`Chemical_Id`, `Name`, `Quantity_Available_ml`, `Quantity_Available_mg`, `Original_Amt`) VALUES
+(1, 'Acetic Anhydride', 0, NULL, 12.4),
+(2, 'Methyl Ethyl Ketone', NULL, 0, 14.2),
+(3, 'Potassium Permanganate', NULL, 12.4, 54.12),
+(4, 'Norephedrine', NULL, 77.2, 101.2),
+(5, 'Ethyl Ether', NULL, 44.2, 54.37),
+(6, 'Isosafrole', 12.3, NULL, 20.4);
 
 -- --------------------------------------------------------
 
@@ -103,29 +88,34 @@ CREATE TABLE `glasswares` (
 --
 
 INSERT INTO `glasswares` (`Glassware_Id`, `Name`, `Quantity_Available`) VALUES
-(1, 'BONG BONG MARCOS', 6),
-(2, 'Hammer', 3),
-(3, 'Lofi Stereo', 2);
+(4, 'Scoopula', 0),
+(5, 'Bunsen Burner', 4),
+(6, 'Crucible Tongs', 12),
+(7, 'Erlenmeyer Flask', 2),
+(8, 'Volumetric Flask', 0),
+(9, 'Graduated Cylinder', 7),
+(10, 'Pipet Bulb', 8),
+(11, 'Ring Clamp', 9);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `group`
+-- Table structure for table `group_table`
 --
 
-CREATE TABLE `group` (
+CREATE TABLE `group_table` (
   `Group_Id` int(11) NOT NULL,
   `Professor` varchar(256) NOT NULL,
   `Subject` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `group`
+-- Dumping data for table `group_table`
 --
 
-INSERT INTO `group` (`Group_Id`, `Professor`, `Subject`) VALUES
-(1, 'Sir Lee Javellana', 'Cmsc 128'),
-(2, 'Ma''am Ash Balangcod', 'Cmsc 125');
+INSERT INTO `group_table` (`Group_Id`, `Professor`, `Subject`) VALUES
+(1, 'Sir Lee Javellana', 'Cmsc 190'),
+(2, 'Ma''am Ash Balangcod', 'Cmsc 123');
 
 -- --------------------------------------------------------
 
@@ -170,37 +160,25 @@ CREATE TABLE `transaction` (
   `Group_Id` int(11) NOT NULL,
   `Qty_Borrowed_Glasswares` int(11) NOT NULL,
   `Qty_Borrowed_Chemicals_ml` float NOT NULL,
-  `Qty_Borrowed_Chemicals_mg` float NOT NULL
+  `Qty_Borrowed_Chemicals_mg` float NOT NULL,
+  `Date_Borrowed` date NOT NULL,
+  `Date_Returned` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `transaction`
 --
 
-INSERT INTO `transaction` (`Trans_Id`, `Glassware_Id`, `Chemical_Id`, `Group_Id`, `Qty_Borrowed_Glasswares`, `Qty_Borrowed_Chemicals_ml`, `Qty_Borrowed_Chemicals_mg`) VALUES
-(5, 1, NULL, 1, 1, 0, 0),
-(6, NULL, 4, 2, 0, 4, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction_history`
---
-
-CREATE TABLE `transaction_history` (
-  `Trans_History_Id` int(11) NOT NULL,
-  `Trans_Id` int(11) NOT NULL,
-  `Date_Borrowed` date NOT NULL,
-  `Date_Returned` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `transaction_history`
---
-
-INSERT INTO `transaction_history` (`Trans_History_Id`, `Trans_Id`, `Date_Borrowed`, `Date_Returned`) VALUES
-(1, 5, '2018-04-09', '2018-04-10'),
-(2, 6, '2018-04-08', '2018-04-08');
+INSERT INTO `transaction` (`Trans_Id`, `Glassware_Id`, `Chemical_Id`, `Group_Id`, `Qty_Borrowed_Glasswares`, `Qty_Borrowed_Chemicals_ml`, `Qty_Borrowed_Chemicals_mg`, `Date_Borrowed`, `Date_Returned`) VALUES
+(5, NULL, 1, 1, 0, 12.4, 0, '2018-04-10', '2018-04-10'),
+(6, NULL, 2, 2, 0, 0, 14.2, '2018-04-22', '2018-04-22'),
+(7, NULL, 3, 2, 0, 0, 41.72, '2017-12-12', '2017-12-12'),
+(8, NULL, 4, 1, 0, 24, 0, '2017-11-01', '2017-11-01'),
+(9, NULL, 5, 1, 0, 0, 10.17, '2018-01-11', '2018-01-11'),
+(10, NULL, 6, 2, 0, 8.1, 0, '2018-04-03', '2018-04-03'),
+(11, 4, NULL, 1, 3, 0, 0, '2018-04-17', NULL),
+(12, 9, NULL, 2, 2, 0, 0, '2018-04-15', '2018-04-18'),
+(13, 11, NULL, 2, 2, 0, 0, '2018-04-26', '2018-04-30');
 
 -- --------------------------------------------------------
 
@@ -231,15 +209,8 @@ INSERT INTO `user_accounts` (`User_Id`, `Staff_Id`, `Username`, `Password`, `sta
 -- Indexes for table `borrower`
 --
 ALTER TABLE `borrower`
-  ADD PRIMARY KEY (`Borrower_Id`);
-
---
--- Indexes for table `borrower_group`
---
-ALTER TABLE `borrower_group`
-  ADD PRIMARY KEY (`Borrower_Group_Id`),
-  ADD KEY `Group_Id` (`Group_Id`),
-  ADD KEY `Borrower_Id` (`Borrower_Id`);
+  ADD PRIMARY KEY (`Borrower_Id`),
+  ADD KEY `Group_Id` (`Group_Id`);
 
 --
 -- Indexes for table `chemicals`
@@ -254,9 +225,9 @@ ALTER TABLE `glasswares`
   ADD PRIMARY KEY (`Glassware_Id`);
 
 --
--- Indexes for table `group`
+-- Indexes for table `group_table`
 --
-ALTER TABLE `group`
+ALTER TABLE `group_table`
   ADD PRIMARY KEY (`Group_Id`);
 
 --
@@ -282,13 +253,6 @@ ALTER TABLE `transaction`
   ADD KEY `Group_Id` (`Group_Id`);
 
 --
--- Indexes for table `transaction_history`
---
-ALTER TABLE `transaction_history`
-  ADD PRIMARY KEY (`Trans_History_Id`),
-  ADD KEY `Trans_Id` (`Trans_Id`);
-
---
 -- Indexes for table `user_accounts`
 --
 ALTER TABLE `user_accounts`
@@ -305,11 +269,6 @@ ALTER TABLE `user_accounts`
 ALTER TABLE `borrower`
   MODIFY `Borrower_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `borrower_group`
---
-ALTER TABLE `borrower_group`
-  MODIFY `Borrower_Group_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
 -- AUTO_INCREMENT for table `chemicals`
 --
 ALTER TABLE `chemicals`
@@ -318,11 +277,11 @@ ALTER TABLE `chemicals`
 -- AUTO_INCREMENT for table `glasswares`
 --
 ALTER TABLE `glasswares`
-  MODIFY `Glassware_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Glassware_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
--- AUTO_INCREMENT for table `group`
+-- AUTO_INCREMENT for table `group_table`
 --
-ALTER TABLE `group`
+ALTER TABLE `group_table`
   MODIFY `Group_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `staff`
@@ -333,12 +292,7 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `Trans_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `transaction_history`
---
-ALTER TABLE `transaction_history`
-  MODIFY `Trans_History_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Trans_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `user_accounts`
 --
@@ -349,11 +303,10 @@ ALTER TABLE `user_accounts`
 --
 
 --
--- Constraints for table `borrower_group`
+-- Constraints for table `borrower`
 --
-ALTER TABLE `borrower_group`
-  ADD CONSTRAINT `borrower_group_ibfk_1` FOREIGN KEY (`Group_Id`) REFERENCES `group` (`Group_Id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `borrower_group_ibfk_2` FOREIGN KEY (`Borrower_Id`) REFERENCES `borrower` (`Borrower_Id`) ON UPDATE CASCADE;
+ALTER TABLE `borrower`
+  ADD CONSTRAINT `borrower_ibfk_1` FOREIGN KEY (`Group_Id`) REFERENCES `group_table` (`Group_Id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `page_permissions`
@@ -367,13 +320,7 @@ ALTER TABLE `page_permissions`
 ALTER TABLE `transaction`
   ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`Glassware_Id`) REFERENCES `glasswares` (`Glassware_Id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`Chemical_Id`) REFERENCES `chemicals` (`Chemical_Id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`Group_Id`) REFERENCES `group` (`Group_Id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `transaction_history`
---
-ALTER TABLE `transaction_history`
-  ADD CONSTRAINT `transaction_history_ibfk_1` FOREIGN KEY (`Trans_Id`) REFERENCES `transaction` (`Trans_Id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`Group_Id`) REFERENCES `group_table` (`Group_Id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user_accounts`
