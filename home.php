c41515f771b8f7b281076939f7b7b191cdca9a65
Karl Mabutas <31471891+karlbenson@users.noreply.github.com>
2018-04-12 21:20:55 +0800

    ayos lamang yaman din lamang at stressdt ako

--- home.php
+++ home.php
@@ -23,7 +23,13 @@
 	<title>Home: UPB Glasswares and Chemicals Inventory</title>
 	<link rel="stylesheet" type="text/css" href="css/home.css">
 	<?php 'loading head';include("head.php"); ?>
+	<script src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/date_time.js"></script>
+	<script src="datatables/DataTables/js/jquery.dataTables.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" charset="utf8" src="datatables/DataTables/js/dataTables.bootstrap4.js"></script>
+	<link rel="stylesheet" href="datatables/DataTables/css/dataTables.bootstrap4.css">
+
 	
 </head>
 <body>
@@ -48,17 +54,18 @@
 					<button class="btn btn-primary" style="cursor: pointer;"><i class="fas fa-list-alt" style="font-size: 50px;"></i><br/>Transaction Table</button>
 			</div>
 		
-			<div class="row">
-				<div class="col">
+			<div class="row justify-content-between">
+				<div class="col text-center">
 					<h1 class="jumbotron-fluid text-center py-4" style="font-size: 50px"><em>Not In Stock</h1>
 				</div>
-				<div class="col">
+				<div class="col text-center">
 					<h1 class="jumbotron-fluid text-center py-4" style="font-size: 50px"><em>Insufficient Quantities</h1>
 				</div>
 			</div>
-			<div class="row justify-content-between" style="height: 350px; overflow-y: scroll;">
-				<div class="col" style="background-color: #edeef2; border-radius: 10px; padding: 30px;">
-	      			<table class="table table-sm table-striped table-condensed table-hover">
+			<div class="row justify-content-between">
+				<div class="col">
+					<div style="background-color: #edeef2; border-radius: 10px; padding: 20px; height: 350px; overflow-y: scroll;">
+	      			<table class="table table-striped table-condensed table-hover" id="nistab">
 	      				<thead class="text-center">
 	      					<tr>
 	      						<th>ID</th>
@@ -99,35 +106,62 @@
 								?>
 	      				</tbody>
 	      			</table>
-	      			<p>
-	      				<ul>
-								
-						</ul>
-	      			</p>
+					</div>	
 				</div>
-				<div class="col" style="background-color: #edeef2; border-radius: 10px; height: 350px; overflow-y: scroll;">
-	      			<p>
-						<ul>
-							<?php
-							$MySearchQuery = "SELECT * FROM chemicals WHERE Quantity_Available_ml < 0.3*(Original_Amt) OR Quantity_Available_mg < 0.3*(Original_Amt)";
-							$MyValues = $MyConnection -> query($MySearchQuery);
-							if (($MyValues -> num_rows) > 0)
-							{
-								while ($MyResults = $MyValues -> fetch_assoc()) //from transaction table
+				
+				<div class="col">
+					<div style="background-color: #edeef2; border-radius: 10px; padding: 20px; height: 350px; overflow-y: scroll;">
+						<table class="table table-striped table-condensed table-hover" id="nistab2">
+	      				<thead class="text-center">
+	      					<tr>
+	      						<th>ID</th>
+	      						<th>Item Type</th>
+	      						<th>Name</th>
+	      						<th>Quantity Left</th>
+	      					</tr>
+	      				</thead>
+	      				<tbody>
+		      					<?php
+		      					
+								$MySearchQuery = "SELECT * FROM glasswares WHERE Quantity_Available < 5 AND Quantity_Available > 0";
+								$MyValues = $MyConnection -> query($MySearchQuery);
+								if (($MyValues -> num_rows) > 0)
 								{
-									echo '<li>'.$MyResults['Name'].' (';						
-									if (is_null($MyResults['Quantity_Available_ml'])){
-										echo $MyResults['Quantity_Available_mg'].' mg)';
-									}else{
-										echo $MyResults['Quantity_Available_ml'].' ml)';
-									}
+										while ($MyResults = $MyValues -> fetch_assoc()) //from transaction table 
+										{
+											echo "<tr>";
+											echo '<td>'.$MyResults['Glassware_Id'].'</td>';
+											echo '<td> Glassware </td>';
+											echo '<td>'.$MyResults['Name'].'</td>';
+											echo '<td>'.$MyResults['Quantity_Available'].' pc/s</td>';
+											echo "</tr>";
+										}
 								}
-							}	
-							?>
-						</ul>
-					</p>
+								
+								$MySearchQuery = "SELECT * FROM chemicals WHERE (Quantity_Available_ml < 0.3*(Original_Amt) OR Quantity_Available_mg < 0.3*(Original_Amt)) AND Quantity_Available_mg > 0 AND Quantity_Available_ml > 0";
+								$MyValues = $MyConnection -> query($MySearchQuery);
+								if (($MyValues -> num_rows) > 0)
+								{
+									while ($MyResults = $MyValues -> fetch_assoc()) //from transaction table
+									{
+										echo "<tr>";					
+										echo '<td>'.$MyResults['Chemical_Id'].'</td>';
+										echo '<td> Chemical </td>';
+										echo '<td>'.$MyResults['Name'].'</td>';
+										echo '<td>';
+										if (is_null($MyResults['Quantity_Available_ml'])){
+											echo $MyResults['Quantity_Available_mg'].' mg</td>';
+										}else{
+											echo $MyResults['Quantity_Available_ml'].' ml</td>';
+										}	
+										echo "</tr>";
+									}
+								}	
+								?>
+	      				</tbody>
+	      				</table>
+					</div>
 				</div>
-			</div>
 		</div>
 		<div class="container">
 			<h1 class="jumbotron-fluid text-center py-4" style="font-size: 50px"><em>Liabilities</h1>
@@ -139,7 +173,7 @@
 							<th>Last Name</th>
 							<th>First Name</th>
 							<th>No. of Items</th>
-							<th>Details</th>
+							<th class="text-center">Details</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -151,10 +185,14 @@
 										if (($MyValues -> num_rows) > 0)
 										{
 											while ($MyResults = $MyValues -> fetch_assoc()) //from transaction table
-											{											
-												echo '<li>'.$MyResults['Last_Name'].', '.$MyResults['First_Name'].' ('.$MyResults['Amt_of_transactions'].')';
-												
-												
+											{		
+												echo "<tr>";
+												echo '<td>'.$MyResults['Student_Number'].'</td>';
+												echo '<td>'.$MyResults['Last_Name'].'</td>';
+												echo '<td>'.$MyResults['First_Name'].'</td>';
+												echo '<td>'.$MyResults['Amt_of_transactions'].' pc/s</td>';
+												echo '<td class="text-center"><button class="btn btn-success" style="cursor:pointer;">See Details</button></td>';
+												echo "</tr>";
 											}
 										}	
 									?>
@@ -308,7 +346,18 @@
 			
 		</div>
 	</div>
-
+	<script>
+		$('#nistab').DataTable({
+			"searching":false,
+			"paging":false,
+			"info": false
+		});
+		$('#nistab2').DataTable({
+			"searching":false,
+			"paging":false,
+			"info":false
+		});
+	</script>
 	
 </body>
 </html>
