bf14bab74f6787c283354d0a9c5588b5f47fea38
Kiana Alessandra Villaera <kvvillaera@up.edu.ph>
2018-04-26 09:45:43 +0800

    Add files via upload

--- home.php
+++ home.php
@@ -2,15 +2,12 @@
 	session_start();
 	
 	error_reporting(0);
-
 	//Server Credentials
 	$MyServerName = "localhost";
 	$MyUserName = "root";
 	$MyPassword = "";
-
 	//Database
 	$MyDBName = 'chem_glasswares';
-
 	$MyConnection = mysqli_connect($MyServer, $MyUserName, $MyPassword, $MyDBName);
 	
 	//include("verify.php");
@@ -34,7 +31,7 @@
 </head>
 <body>
 		<div class="container">
-			<div class="row container-fluid" style="color: black; background-color: #edeef2; border-radius: 5px; margin-bottom: 50px;">
+			<div class="row container-fluid" style="color: black; background-color: #edeef2; border-radius: 5px; margin-bottom: 50px; border:2px solid #dbdbdb;">
 				<div class="col-sm-4 timestamp" style="text-align: left !important;">
 					<span id="date_time" style="color:black;"></span>
 					<script type="text/javascript">window.onload = date_time('date_time');</script>
@@ -64,7 +61,7 @@
 			</div>
 			<div class="row justify-content-between">
 				<div class="col">
-					<div style="background-color: #edeef2; border-radius: 10px; padding: 20px; height: 350px; overflow-y: scroll;">
+					<div style="background-color: #edeef2; border-radius: 10px; padding: 20px; height: 350px; overflow-y: scroll; border:2px solid #dbdbdb;">
 	      			<table class="table table-striped table-condensed table-hover" id="nistab">
 	      				<thead class="text-center">
 	      					<tr>
@@ -110,7 +107,7 @@
 				</div>
 				
 				<div class="col">
-					<div style="background-color: #edeef2; border-radius: 10px; padding: 20px; height: 350px; overflow-y: scroll;">
+					<div style="background-color: #edeef2; border-radius: 10px; border-color:#d3d3d3 ; padding: 20px; height: 350px; overflow-y: scroll;border:2px solid #dbdbdb;">
 						<table class="table table-striped table-condensed table-hover" id="nistab2">
 	      				<thead class="text-center">
 	      					<tr>
@@ -163,10 +160,11 @@
 					</div>
 				</div>
 		</div>
-		<div class="container">
+		<div class="container-fluid" style="padding-top :35px;padding-bottom :35px; margin-top:35px;margin-bottom:35px; background-color: #edeef2; border-radius: 10px; border:2px solid #dbdbdb;">
+			<div class="container" style="padding: 10px;">
 			<h1 class="jumbotron-fluid text-center py-4" style="font-size: 50px"><em>Liabilities</h1>
 			<div>
-				<table class="table" style="background-color: white;">
+				<table class="table table-striped table-hover" id="tab_lia">
 					<thead class="text-center">
 						<tr>
 							<th>Student Number</th>
@@ -177,10 +175,8 @@
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>
 								<?php
-										$MySearchQuery = "SELECT * FROM borrower WHERE Amt_of_transactions > 0";
+										$MySearchQuery = "SELECT * FROM borrower WHERE Amt_of_transactions > 0 LIMIT 5";
 										$MyValues = $MyConnection -> query($MySearchQuery);
 										if (($MyValues -> num_rows) > 0)
 										{
@@ -190,26 +186,54 @@
 												echo '<td>'.$MyResults['Student_Number'].'</td>';
 												echo '<td>'.$MyResults['Last_Name'].'</td>';
 												echo '<td>'.$MyResults['First_Name'].'</td>';
-												echo '<td>'.$MyResults['Amt_of_transactions'].' pc/s</td>';
-												echo '<td class="text-center"><button class="btn btn-success" style="cursor:pointer;">See Details</button></td>';
+												echo '<td>'.$MyResults['Amt_of_transactions'].' transaction/s</td>';
+												/*
+													echo '
+												<form role="form" action = "clear_liability.php" method="POST">
+												<input class="hide" name="Student_Number" type="text" value="'.$MyResults["Student_Number"].'" />
+												<input class="hide" name="Last_Name" type="text" value="'.$MyResults["Last_Name"].'" />
+												<input class="hide" name="First_Name" type="text" value="'.$MyResults["First_Name"].'" />
+												<td class="text-center"><input id="see_details" class="btn btn-success" style="cursor:pointer;" type="submit" value="See Details" /></td>
+												</form>';
+												*/
+												echo '
+												
+												<td class="text-center">
+													<button class="openmodal btn btn-success ">See Details</button>
+												</td>
+
+											
+												<div class="modal see_details_modal">
+												
+													<div class="modal-content">
+														<span class="close_modal" >&times;</span>
+														<h3><strong>'.$MyResults['First_Name'].' '.$MyResults['Last_Name'].'</strong> </h3><br>
+														Student Number: '. $MyResults['Student_Number'].' <br><br>
+
+														
+
+													</div>
+												</div>
+												';
 												echo "</tr>";
 											}
 										}	
 									?>
-							</td>
-						</tr>
 					</tbody>
 				</table>
 			</div>
 		</div>
-		<div  >
+		<div class="col-sm-12 text-center">
+			<button class="btn btn-primary" style="cursor: pointer;"> SEE FULL TABLE </button>
+		</div>
+		<div>
 		
-			<div class="col-lg-12" style="background-color:white">
+			<div class="col-lg-12">
 			
-				<h1 class="jumbotron-fluid text-center py-4" style="font-size: 50px"><em>Transaction History for Glassware</h1>
+				<h1 class="jumbotron-fluid text-center py-4" style="font-size: 50px; margin-top:35px;"><em>Transaction History for Glassware</h1>
 	    	
-	    		<div class="container" style="background-color:white">
-			          		<table class="table" style="background-color:white">
+	    		<div class="container" style="margin-bottom:35px;">
+			          		<table class="table table-striped table-hover" id="tab_gla">
 			          			<thead class="text-center" >
 					            	<tr>
 						                <th>Glassware Borrowed</th>
@@ -225,7 +249,7 @@
 					            <tbody >
 								
 									<?php
-										$MySearchQuery = "SELECT * FROM transaction JOIN glasswares USING (Glassware_Id) ORDER BY transaction.Date_Returned";
+										$MySearchQuery = "SELECT * FROM transaction JOIN glasswares USING (Glassware_Id) ORDER BY transaction.Date_Returned LIMIT 5";
 										$MyValues = $MyConnection -> query($MySearchQuery);
 										if (($MyValues -> num_rows) > 0)
 										{
@@ -277,10 +301,13 @@
 					        	</tbody>
 					        </table>
 				</div>
+				<div class="col-sm-12 text-center">
+					<button class="btn btn-primary" style="cursor: pointer;"> SEE FULL TABLE </button>
+				</div>
 				
-				<h1 class="jumbotron-fluid text-center py-4" style="font-size: 50px"><em>Transaction History for Chemicals</h1>
-				<div class="container" style="background-color:white">
-			          		<table class="table" style="background-color:white">
+				<h1 class="jumbotron-fluid text-center py-4" style="font-size: 50px; margin-top:35px;"><em>Transaction History for Chemicals</h1>
+				<div class="container" style="margin-bottom:35px;">
+			          		<table class="table table-striped table-hover" id="tab_che">
 			          			<thead class="text-center" >
 					            	<tr>
 						                <th>Chemical Requested</th>
@@ -295,7 +322,7 @@
 					            </thead>
 					            <tbody >
 									<?php
-										$MySearchQuery = "SELECT * FROM transaction JOIN chemicals USING (Chemical_Id) ORDER BY transaction.Date_Returned";
+										$MySearchQuery = "SELECT * FROM transaction JOIN chemicals USING (Chemical_Id) ORDER BY transaction.Date_Returned LIMIT 5";
 										$MyValues = $MyConnection -> query($MySearchQuery);
 										if (($MyValues -> num_rows) > 0)
 										{
@@ -340,10 +367,13 @@
 					        	</tbody>
 					        </table>
 				</div>
-						
+				<div class="col-sm-12 text-center">
+					<button class="btn btn-primary" style="cursor: pointer;"> SEE FULL TABLE </button>
+				</div>	
 			</div>	
 			
 			
+			</div>
 		</div>
 	</div>
 	<script>
@@ -357,7 +387,64 @@
 			"paging":false,
 			"info":false
 		});
+		$('#tab_lia').DataTable({
+			"searching":false,
+			"paging":false,
+			"info":false,
+		"columns":
+		[
+			null,
+		    null,
+		    null,	
+		    { "orderable": false },
+		    { "orderable": false }
+		]
+		});
+		$('#tab_gla').DataTable({
+			"searching":false,
+			"paging":false,
+			"info":false,
+		"columns":
+		[
+			null,
+			null,
+		    null,
+		    null,
+		    null,
+		    null,
+		    null
+		]
+		});
+		$('#tab_che').DataTable({
+			"searching":false,
+			"paging":false,
+			"info":false,
+		});
+
+		//-------------Modal scripting
+		var modals = document.getElementsByClassName('modal');
+		var btns = document.getElementsByClassName("openmodal");
+		var spans=document.getElementsByClassName("close_modal");
+
+		for(let i=0;i<btns.length;i++){
+   			 btns[i].onclick = function() {
+        		modals[i].style.display = "block";
+    		}
+		}
+
+		for(let i=0;i<spans.length;i++){
+    		spans[i].onclick = function() {
+        		modals[i].style.display = "none";
+    		}	
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		    if (event.target == modal) {
+		        modal.style.display = "none";
+		    }
+		}
 	</script>
-	
+	<?php include("footer.php")  ?>
 </body>
 </html>
