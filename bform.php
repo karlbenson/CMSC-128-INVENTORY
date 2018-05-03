f09b8eae34af18988c041ec385ddc9496b65077e
Karl Mabutas <31471891+karlbenson@users.noreply.github.com>
2018-05-03 10:43:33 +0800

    changed eneter functionality on bform

--- bform.php
+++ bform.php
@@ -268,9 +268,28 @@
 
   });
 
+  $(document).on('keydown', 'document', function(event) {
+    var key=event.which;
+    if (key==13) {
+      $("#confirm").click();
+    }
+  });
+
   $('#clonegrp').on('keyup','#item', function() {
-    $(this).closest('.grpit').find('#max').val('text');
-    $(this).closest('.grpit').find('#unit').val('text');
+    if ($(this).closest('.grpit').find('#item').val()=='') {
+      $(this).closest('.grpit').find('#max').val('');
+      $(this).closest('.grpit').find('#unit').val('');
+    }else{
+      <?php 
+        $query2="SELECT Name FROM glasswares;";
+        $row2=mysqli_query($MyConnection,$query);
+        $result2=mysqli_fetch_array($row,MYSQLI_ASSOC);
+      ?>
+      alert("<?php json_encode($result2[0])?>");
+      $(this).closest('.grpit').find('#max').val('catch');
+      $(this).closest('.grpit').find('#unit').val('catch');
+    }
+    
   });
 </script>
 
