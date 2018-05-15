b2ced75646a2225ef87c850eff97455a6b79a26b
Karl Mabutas <31471891+karlbenson@users.noreply.github.com>
2018-05-15 21:58:58 +0800

    ohana means homaigawd

--- src/a.php
+++ src/a.php
@@ -1,4 +1,19 @@
-Ralston Mark Chan<br/>
-Kiana Alessandra Villaera <br/>
-Bernadette Gennove <br/>
-Karlvinzon Mabutas <br/>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		CREDITS
+	</title>
+</head>
+<body>
+	<center>
+		<img src="credits.png" style="width: 800px; padding-top: 70px;">
+	</center>
+	<center>
+		Ralston Mark Chan<br/>
+		Kiana Alessandra Villaera <br/>
+		Bernadette Genove <br/>
+		Karlvinzon Mabutas <br/>
+	</center>
+</body>
+</html>
